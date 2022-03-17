6ec986149d27243d39c302e878597923a178999d
Błażej Domagała <petitoff@outlook.com>
2022-03-17 17:41:04 +0100

    Znajdowanie elementy odwrotnego

--- octave/abc.m
+++ octave/abc.m
@@ -1,28 +1,11 @@
 function wynik = abc(x,n)
-  if(x<n)
-    z = x;
-    x = n;
-    n = z;
-  endif
-  
-  koniecn = 0;
-  while koniecn != 1
-    disp(x);
-    disp(n);
-    t = idivide(x, int32(n), "fix");
-    koniecn = mod(x, (t*n));
-    
-    if koniecn == 0
-      n = "Brak rozwiazania!";
-      break;
+  for i = 1:n
+    y = x * i;
+    wynikMod = mod(y,n);
+    if(wynikMod == 1)
+      wynik = i;
+      return;
     endif
-    
-    if koniecn == 1
-      break;
-    endif
-    
-    x = t;
-    n = koniecn;
-  endwhile
-  wynik = n;
+  endfor
+  disp("Brak elementu odwrotnego");
 endfunction
