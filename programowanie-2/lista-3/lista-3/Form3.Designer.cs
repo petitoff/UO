6305c875c0dbf48c4eb4d9021db432c558030391
Błażej Domagała <petitoff@outlook.com>
2022-03-31 18:55:51 +0000

    Dodanie scrolla do obu listboxów

--- programowanie-2/lista-3/lista-3/Form3.Designer.cs
+++ programowanie-2/lista-3/lista-3/Form3.Designer.cs
@@ -78,6 +78,7 @@ namespace lista_3
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
             this.listBox1.Location = new System.Drawing.Point(154, 221);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(493, 199);
