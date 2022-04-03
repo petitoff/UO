08c4ad6f3b6d83c5ec8dcccf0379fe89778c75f2
Błażej Domagała <petitoff@outlook.com>
2022-04-03 22:27:10 +0200

    Program splatający dwie zmienne w jedną
    Program bierze po kolei char z tablicy i łączy char

---
+++ assembly/Lista 2/zadanie-3/zadanie-3/zadanie-3.cpp
@@ -0,0 +1,34 @@
+
+#include "stdafx.h"
+
+char napis1[] = "asd";
+char napis2[] = "fgh";
+char napis3[] = "";
+
+int main()
+{
+	__asm
+	{
+		mov EAX, offset napis3
+		mov EBX, offset napis1
+		mov ECX, offset napis2
+
+		start : mov DL, [EBX]
+				cmp DL, 0
+				jz end
+				mov[EAX], DL
+				inc EAX
+
+				mov DL, [ECX]
+				mov[EAX], DL
+
+				inc EBX
+				inc ECX
+				inc EAX
+				jmp start
+				end :
+	}
+	std::cout << napis3;
+
+	std::cin.get();
+}
