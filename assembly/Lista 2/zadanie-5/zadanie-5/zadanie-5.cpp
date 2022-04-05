484a97fdaea648ceaa292ea1317f442006412cdb
Błażej Domagała <petitoff@outlook.com>
2022-04-05 18:51:26 +0200

    Przygotowanie plików do zadania

---
+++ assembly/Lista 2/zadanie-5/zadanie-5/zadanie-5.cpp
@@ -0,0 +1,17 @@
+
+#include "stdafx.h"
+
+__int16 wynik;
+
+int main()
+{
+	__asm
+	{
+		mov ax, 10110b
+		shr ax, 1011b
+		and ax, 1b
+		mov [wynik], ax
+	}
+	std::cout << wynik;
+	std::cin.get();
+}
