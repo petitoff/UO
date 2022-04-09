272b768c41861de7a8132551e86705130fe489be
Błażej Domagała <petitoff@outlook.com>
2022-04-09 15:41:45 +0200

    Update zadanie-5.cpp

--- assembly/Lista 2/zadanie-5/zadanie-5/zadanie-5.cpp
+++ assembly/Lista 2/zadanie-5/zadanie-5/zadanie-5.cpp
@@ -1,17 +1,23 @@
 
 #include "stdafx.h"
+#include <bitset>
 
-__int16 wynik;
+__int16 wynik = 0b1010101010101010;
 
 int main()
 {
 	__asm
 	{
-		mov ax, 10110b
+		/*mov ax, 10110b
 		shr ax, 1011b
-		and ax, 1b
-		mov [wynik], ax
+		and ax, 1b*/
+
+		mov AX, wynik
+		shr AX, 1b
+		mov wynik, AX
+
+		//mov [wynik], ax
 	}
-	std::cout << wynik;
+	std::cout << std::bitset<16>(wynik);
 	std::cin.get();
 }
