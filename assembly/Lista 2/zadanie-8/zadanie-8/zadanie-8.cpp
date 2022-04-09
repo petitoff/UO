829ca923de0d2d2608f48b0e556bf778c4fe1b60
Błażej Domagała <petitoff@outlook.com>
2022-04-09 18:45:50 +0000

    Zmiana wielkości zmiennej i rejestrów
    Została użyta zła zmienna względem wymagań w zadaniu co wymusiło zmianę użytych rejestrów

--- assembly/Lista 2/zadanie-8/zadanie-8/zadanie-8.cpp
+++ assembly/Lista 2/zadanie-8/zadanie-8/zadanie-8.cpp
@@ -1,26 +1,26 @@
 ﻿
 #include "stdafx.h"
 
-__int8 t[4] = { 1,2,3,4 };
-short int d = 11;
+__int16 t[4] = { 1,2,3,4 };
+__int8 d = 12;
 
 int main()
 {
 	__asm
 	{
-		mov cx, d
-		mov bx, cx
-		and cx, 1
-		xor cx, 1
+		mov CL, d
+		mov BL, CL
+		and CL, 1
+		xor CL, 1
 
-		mov ax, bx
-		mul cx
-		add t[3], al
+		mov AL, BL
+		mul CL
+		add t[6], AX
 
-		xor cx, 1
-		mov ax, bx
-		mul cx
-		add t[0], al
+		xor CL, 1
+		mov AL, BL
+		mul CL
+		add t[0], AX
 	}
 	std::cout << "Zawartosc tablicy: " << (int)t[0] << ", " << (int)t[1] << ", " << (int)t[2] << ", " << (int)t[3] << std::endl;
 	std::cin.get();
