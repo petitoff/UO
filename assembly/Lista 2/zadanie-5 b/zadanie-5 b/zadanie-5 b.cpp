e003e16d74146b5bf8dd282ca7731418f8577e99
Błażej Domagała <petitoff@outlook.com>
2022-04-09 17:38:41 +0000

    Update zadanie-5 b.cpp

--- assembly/Lista 2/zadanie-5 b/zadanie-5 b/zadanie-5 b.cpp
+++ assembly/Lista 2/zadanie-5 b/zadanie-5 b/zadanie-5 b.cpp
@@ -1,11 +1,101 @@
 
 #include "stdafx.h"
 
+__int16 liczba = 15;
+
 int main()
 {
 	__asm
 	{
+		// Zadanie 5b
+		xor eax, eax
+		xor ebx, ebx
+		xor ecx, ecx
 
+		mov AX, liczba 
+
+		mov AX, 15
+
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+
+		shr AX, 1
+		mov BX, AX
+		and BX, 1
+		add CX, BX
+		mov liczba, cx
 	}
+	std::cout << liczba << std::endl;
 	std::cin.get();
 }
