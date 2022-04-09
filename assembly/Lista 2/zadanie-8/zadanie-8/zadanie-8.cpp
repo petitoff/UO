91a879da0544538c45f1d7ada4ae4d0e3378d7cb
Błażej Domagała <petitoff@outlook.com>
2022-04-09 18:39:25 +0000

    Naprawienie niepoprawnego wstawianai się danych
    Odwoływanie się do miejsc w tablicy było niepoprawne

--- assembly/Lista 2/zadanie-8/zadanie-8/zadanie-8.cpp
+++ assembly/Lista 2/zadanie-8/zadanie-8/zadanie-8.cpp
@@ -2,25 +2,25 @@
 #include "stdafx.h"
 
 __int8 t[4] = { 1,2,3,4 };
-short int d = 10;
+short int d = 11;
 
 int main()
 {
 	__asm
 	{
-		mov cx, d;
-		mov bx, cx;
-		and cx, 1;
-		xor cx, 1;
+		mov cx, d
+		mov bx, cx
+		and cx, 1
+		xor cx, 1
 
-		mov ax, bx;
-		mul cx;
-		add t[0], al;
+		mov ax, bx
+		mul cx
+		add t[3], al
 
-		xor cx, 1;
-		mov ax, bx;
-		mul cx;
-		add t[6], al;
+		xor cx, 1
+		mov ax, bx
+		mul cx
+		add t[0], al
 	}
 	std::cout << "Zawartosc tablicy: " << (int)t[0] << ", " << (int)t[1] << ", " << (int)t[2] << ", " << (int)t[3] << std::endl;
 	std::cin.get();
