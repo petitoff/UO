4cc3eead0cddefb97020c6f66b59f36371b48944
Błażej Domagała <petitoff@outlook.com>
2022-04-09 18:11:58 +0000

    Stworzenie zadania 8

---
+++ assembly/Lista 2/zadanie-8/zadanie-8/zadanie-8.cpp
@@ -0,0 +1,27 @@
+﻿
+#include "stdafx.h"
+
+__int8 t[4] = { 1,2,3,4 };
+short int d = 10;
+
+int main()
+{
+	__asm
+	{
+		mov cx, d;
+		mov bx, cx;
+		and cx, 1;
+		xor cx, 1;
+
+		mov ax, bx;
+		mul cx;
+		add t[0], al;
+
+		xor cx, 1;
+		mov ax, bx;
+		mul cx;
+		add t[6], al;
+	}
+	std::cout << "Zawartosc tablicy: " << (int)t[0] << ", " << (int)t[1] << ", " << (int)t[2] << ", " << (int)t[3] << std::endl;
+	std::cin.get();
+}
