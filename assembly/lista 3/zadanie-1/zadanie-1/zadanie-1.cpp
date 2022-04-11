9f2e2226c444f185203f5873639e116b57c08c77
Błażej Domagała <petitoff@outlook.com>
2022-04-11 10:47:22 +0200

    Rozwiazanie zadania 1

---
+++ assembly/lista 3/zadanie-1/zadanie-1/zadanie-1.cpp
@@ -0,0 +1,25 @@
+﻿
+#include "stdafx.h"
+
+char napis[] = "abcz";
+__int8 index = 0;
+
+int main()
+{
+	__asm
+	{
+		mov EDI, offset napis
+		mov AL, 1
+		petla_tutaj:	mov BL, AL // o ile ma nastąpić przesunięcie znaku
+
+						add AL, 1
+						add[EDI], BL // przesunięcie znaku
+
+						inc EDI // zwiększenie wskaźnika o 1
+						mov BL, [EDI] // przeniesienie bajtu do rejestru BL
+						cmp BL, 0 // sprawdzenie czy jest zerem aby sprawdzić czy to nie koniec string'u
+						jne petla_tutaj // jeżeli nie to wykonuj od początku
+	}
+	std::cout << napis;
+	std::cin.get();
+}
