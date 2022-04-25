1d94c143eb7547cab3890b47fea284224aade616
Błażej Domagała <petitoff@outlook.com>
2022-04-25 14:38:13 +0000

    Przekazanie projektu z listy 4 i odpowiednia zmiana nazw
    Lista 5 opiera się na udoskonaleniu listy 4 i dodaniu kolejnych funkcji

---
+++ programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace BłażejDomagała_Bankowość
+{
+    internal class Transakcja
+    {
+        private string bankName = "eBank";
+        private string currency = "PLN";
+        private float amount;
+        private int id; // indentyfikator transakcji
+        private bool isCompleted;
+        private int userId; // indentyfikator użytkownika
+        private string userFirstName;
+        private string userLastName;
+        private string dateOfTransaction;
+        private int endUserId;
+        private string endUserFirstName;
+        private string endUserLastName;
+        private string endBankName;
+        private string description;
+
+        public Transakcja()
+        {
+            // Konstruktor bezargumentowy
+            this.bankName = "eBank";
+            this.currency = "PLN";
+            this.amount = 120.50F;
+            this.id = 2;
+            this.isCompleted = false;
+            this.userId = 37128;
+            this.userFirstName = "Jan";
+            this.userLastName = "Kowalski";
+            this.dateOfTransaction = "06-04-2022";
+            this.endUserId = 27431;
+            this.endUserFirstName = "Dariusz";
+            this.endUserLastName = "Nowak";
+            this.endBankName = "mBank";
+            this.description = "Przelew";
+        }
+
+        public Transakcja(string nameOfBank, string currency, float amount, int id,bool isCompleted, int idUser, string firstNameUser, string lastNameUser, string dateOfTransaction, int idUserDestination, string firstNameUserDestination, string lastNameUserDestination, string nameOfBankDestination, string description)
+        {
+            // Konstruktor wieloargumentowy
+            this.bankName = nameOfBank;
+            this.currency = currency;
+            this.amount = amount;
+            this.id = id;
+            this.isCompleted = isCompleted;
+            this.userId = idUser;
+            this.userFirstName = firstNameUser;
+            this.userLastName = lastNameUser;
+            this.dateOfTransaction = dateOfTransaction;
+            this.endUserId = idUserDestination;
+            this.endUserFirstName = firstNameUserDestination;
+            this.endUserLastName = lastNameUserDestination;
+            this.endBankName = nameOfBankDestination;
+            this.description = description;
+        }
+
+        public Transakcja(Transakcja t)
+        {
+            this.bankName = t.bankName;
+            this.currency = t.currency;
+            this.amount = t.amount;
+            this.id = t.id;
+            this.isCompleted = t.isCompleted;
+            this.userId = t.userId;
+            this.userFirstName = t.userFirstName;
+            this.userLastName = t.userLastName;
+            this.dateOfTransaction = t.dateOfTransaction;
+            this.endUserId = t.endUserId;
+            this.endUserFirstName = t.endUserFirstName;
+            this.endUserLastName = t.endUserLastName;
+            this.endBankName = t.endBankName;
+            this.description = t.description;
+        }
+
+        public void Write(ListBox listBox)
+        {
+            CheckIfTheSameId();
+            CheckOfDescriptionIsEmpty();
+
+            listBox.Items.Add($"Nazwa banku: {bankName}");
+            listBox.Items.Add($"Nazwa waluty: {currency}");
+            listBox.Items.Add($"Kwota transakcji: {amount}");
+            listBox.Items.Add($"Numer indentyfikacyjny transakcji: {id}");
+            listBox.Items.Add($"Transakcja {(isCompleted == true ? "została" : "niezostała")} zrelizowana");
+            listBox.Items.Add($"Numer indentyfikacyjny użytkownika wysyłającego: {userId}");
+            listBox.Items.Add($"Imię użytkownika wysyłającego: {userFirstName}");
+            listBox.Items.Add($"Nazwisko użytkownika wysyłającego: {userFirstName}");
+            listBox.Items.Add($"Data wykonania transakcji: {dateOfTransaction}");
+            listBox.Items.Add($"Numer indentyfikacyjny użytkownika do którego ma dotrzeć przelew: {endUserId}");
+            listBox.Items.Add($"Imię użytkownika do którego ma dotrzeć przelew: {endUserFirstName}");
+            listBox.Items.Add($"Nazwisko użytkownika do którego ma dotrzeć przelew: {endUserLastName}");
+            listBox.Items.Add($"Nazwa banku użytkownika do którego ma dotrzeć przelew: {endBankName}");
+            listBox.Items.Add($"Opis do transakcji: {description}");
+        }
+
+        private void CheckIfTheSameId()
+        {
+            if(userId == endUserId)
+            {
+                MessageBox.Show("Nie można dokonać transakcji na to samo konto!");
+            }
+        }
+
+        private void CheckOfDescriptionIsEmpty()
+        {
+            if(description == "")
+            {
+                MessageBox.Show("Opis transakcji nie może być pusty!");
+            }
+        }
+
+        ~Transakcja()
+        {
+            MessageBox.Show("Likwidacja obiektu klasy Transakcja.");
+        }
+    }
+}
