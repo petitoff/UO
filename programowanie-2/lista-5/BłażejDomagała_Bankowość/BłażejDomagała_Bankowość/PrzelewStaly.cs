10caef4bf7f134d75593da6f39710eb77355f91b
Błażej Domagała <petitoff@outlook.com>
2022-04-25 16:13:10 +0000

    Dodanie informacji o przelewie
    Dodanie informacji o rodzaju przelewu oraz czy przelew stały jest aktwyny

--- programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/PrzelewStaly.cs
+++ programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/PrzelewStaly.cs
@@ -9,40 +9,40 @@ namespace BłażejDomagała_Bankowość
 {
     internal class PrzelewStaly : Transakcja
     {
-        //private string bankName = "eBank";
-        //private string currency = "PLN";
-        //private float amount;
-        //private int id; // indentyfikator transakcji
-        //private bool isCompleted;
-        //private int userId; // indentyfikator użytkownika
-        //private string userFirstName;
-        //private string userLastName;
-        //private string dateOfTransaction;
-
         string dateWhenTransactionHasToBePerformed;
         string timeWhenTransactionHasToBePerformed;
         string timeZoneGmt;
         bool isActive;
-
-        //private int endUserId;
-        //private string endUserFirstName;
-        //private string endUserLastName;
-        //private string endBankName;
-        //private string description;
+        string typeOfTransaction;
 
         public PrzelewStaly() : base()
         {
             dateWhenTransactionHasToBePerformed = "25-04-2022";
             timeWhenTransactionHasToBePerformed = "10.10";
             timeZoneGmt = "+2";
+            isActive = true;
+            typeOfTransaction = "przelew stały";
         }
 
         public new void Write(ListBox listBox)
         {
+            var transakcja = new Transakcja();
+
             base.Write(listBox);
             listBox.Items.Add(dateWhenTransactionHasToBePerformed);
             listBox.Items.Add(timeWhenTransactionHasToBePerformed);
             listBox.Items.Add(timeZoneGmt);
+            listBox.Items.Add(CheckIsActive(isActive));
+            listBox.Items.Add(transakcja.TypeOfTransaction(typeOfTransaction));
+        }
+
+        private string CheckIsActive(bool isActive)
+        {
+            if (isActive)
+            {
+                return "Przelew stały jest aktywny";
+            }
+            return "Przelew stały jest nieaktywny";
         }
     }
 }
