37ad40cc01332de61aced307c11b8ed92fc6e6c5
Błażej Domagała <petitoff@outlook.com>
2022-04-28 20:56:00 +0000

    Zakomentowanei linikii kodu

--- programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
+++ programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
@@ -13,7 +13,7 @@ namespace BłażejDomagała_Bankowość
     {
         private string bankName = "eBank";
         private string currency = "PLN";
-        private float amount;
+        //private float amount;
         private static int id; // indentyfikator transakcji
         private bool isCompleted;
         private int userId; // indentyfikator użytkownika
