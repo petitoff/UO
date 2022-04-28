3455582fc61941ec06aabd69debf11c02038c963
Błażej Domagała <petitoff@outlook.com>
2022-04-28 20:21:03 +0000

    Dodano polimorfizm

--- programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/PrzelewZwykly.cs
+++ programowanie-2/lista-5/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/PrzelewZwykly.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BłażejDomagała_Bankowość
 {
-    internal class PrzelewZwykly
+    internal class PrzelewZwykly : Transakcja
     {
+
+        public PrzelewZwykly() : base()
+        {
+
+        }
+
+        public new void Write(ListBox listBox)
+        {
+            base.Write(listBox);
+        }
     }
 }
