6f12940f80bf120f17ac8d3f763b9469d0960c01
Błażej Domagała <petitoff@outlook.com>
2022-04-29 14:33:46 +0200

    Create Transakcja.cs

---
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BłażejDomagała_Bankowość
+{
+    class Transakcja
+    {
+        private string bankName = "eBank";
+        private string currency = "PLN";
+        private static int id = 0;
+        private DateTime dateofTransaction;
+
+        private int userId;
+        private string userFirstName;
+        private string userLastName;
+
+        private int endUserId;
+        private string endUserFirstName;
+        private string endUserLastName;
+
+        public Transakcja()
+        {
+            id += 1;
+            this.userId = 0;
+            this.endUserId = userId;
+            this.userFirstName = "-";
+            this.userLastName = "-";
+            this.endUserFirstName = "-";
+            this.endUserLastName = "-";
+        }
+    }
+}
