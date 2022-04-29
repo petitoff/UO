fb6dae3f3983e90e265ddae27978b18604c75fbc
Błażej Domagała <petitoff@outlook.com>
2022-04-29 14:48:02 +0200

    Dodanie zmiennych
    Zmienne przechowujące informacje o transakcji

--- programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Transakcja.cs
@@ -13,10 +13,18 @@ namespace BłażejDomagała_Bankowość
         private string bankName; // nazwa banku z którego wychodzi przelew
         private string endBankName; // nazwa banku do którego idzie
 
-        private int userId;
-        private string userFirstName;
-        private string userLastName;
+        private string currency = "PLN"; // symbol waluty w jakiej dociera przelew
+        private static int id = 0; // id transakcji
+        private DateTime dateofTransaction; // data wysłania przelewu
+        private string description; // opis przelewu od użytkownika
+        private string isCompleted; // czy transakcja została już zrealizowana
 
+        // dane użytkownika wyjściowego
+        private int userId; // id użytkownika wysyłającego przelew
+        private string userFirstName; // imie użytkownika wyjściowego
+        private string userLastName; // nazwisko użytkownika wyjściowego
+
+        // dane użytkownika wejściowego
         private int endUserId;
         private string endUserFirstName;
         private string endUserLastName;
