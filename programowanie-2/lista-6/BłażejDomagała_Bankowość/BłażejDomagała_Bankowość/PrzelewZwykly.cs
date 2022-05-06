02a569f5d656a91789517e85d2d9199086d49a6b
Błażej Domagała <petitoff@outlook.com>
2022-05-06 15:03:32 +0200

    Create PrzelewZwykly.cs

---
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/PrzelewZwykly.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BłażejDomagała_Bankowość
+{
+    class PrzelewZwykly
+    {
+        private List<string> categoriesList = new List<string>{"jedzenie", "zakupy", "paliwo"};
+
+    }
+}
