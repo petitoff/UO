439bf49a48940b70aac3c849efae701912ea0654
Błażej Domagała <petitoff@outlook.com>
2022-05-06 15:03:37 +0200

    Create GetInputValidator.cs

---
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/GetInputValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BłażejDomagała_Bankowość
+{
+    class GetInputValidator
+    {
+        public static float ConvertToFloat(string s)
+        {
+            try
+            {
+                return Single.Parse(s);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Błąd! Wprowadź liczby zamiast tekstu!");
+                return 0;
+            }
+        }
+
+        public static DateTime GetOnlyDDMMYYY(DateTimePicker dateTimePicker)
+        {
+            //MessageBox.Show(Convert.ToDateTime(dateTimePicker.Value.ToShortDateString()).ToString("dd/MM/yyyy"));
+            //DateTime dt = Convert.ToDateTime(dateTimePicker.Value.ToShortDateString());
+
+            return Convert.ToDateTime(dateTimePicker.Value.ToShortDateString()); ;
+        }
+    }
+}
