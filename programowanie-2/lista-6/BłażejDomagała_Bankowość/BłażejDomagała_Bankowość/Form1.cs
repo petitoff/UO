218a47ac7a8783a97c9a7e8cd1227abc3d63a021
Błażej Domagała <petitoff@outlook.com>
2022-05-06 15:04:58 +0200

    Dodanie obsługi wejścia informacji o transakcji od użytkownika

--- programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.cs
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.cs
@@ -23,8 +23,23 @@ namespace BłażejDomagała_Bankowość
             transakcja.Write(listBox1);
             listBox1.Items.Add("");
 
-            var transakcja2 = new Transakcja("eBank", "mBank", 785F);
+            var transakcja2 = new Transakcja("eBank", "mBank", 785F, "PLN", DateTime.Now, "Przelew");
             transakcja2.Write(listBox1);
+            listBox1.Items.Add("");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            var transakcja = new Transakcja(textBox1.Text, textBox2.Text, GetInputValidator.ConvertToFloat(textBox3.Text), comboBox1.Text, GetInputValidator.GetOnlyDDMMYYY(dateTimePicker1), textBox4.Text);
+            transakcja.Write(listBox1);
+            listBox1.Items.Add("");
         }
     }
 }
