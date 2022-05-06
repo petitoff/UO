5f110194adab89161f29d4a52594ae54eacdbe98
Błażej Domagała <petitoff@outlook.com>
2022-05-06 15:04:29 +0200

    Przykładowe dane do wypełniania

--- programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form2.cs
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form2.cs
@@ -16,5 +16,22 @@ namespace BłażejDomagała_Bankowość
         {
             InitializeComponent();
         }
+
+        private DateTime dateOfTransaction;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrzelewStaly przelewStaly = new PrzelewStaly();
+            przelewStaly.Write(listBox1);
+            listBox1.Items.Add("");
+            PrzelewStaly przelewStaly2 = new PrzelewStaly("eBank", "mBank", 785F, "PLN", 1000F, dateOfTransaction, "przelew");
+            przelewStaly2.Write(listBox1);
+            listBox1.Items.Add("");
+        }
+
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+            dateOfTransaction = dateTimePicker1.Value;
+        }
     }
 }
