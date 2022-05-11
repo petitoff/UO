aa17cabc381b195468e4543eb04160f30a9f28bf
Błażej Domagała <petitoff@outlook.com>
2022-05-11 20:46:43 +0200

    Form handling

--- programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.cs
+++ programowanie-2/lista-6/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.cs
@@ -17,26 +17,34 @@ namespace BłażejDomagała_Bankowość
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var transakcja = new Transakcja();
-            transakcja.Write(listBox1);
-            listBox1.Items.Add("");
-
-            var transakcja2 = new Transakcja("eBank", "mBank", 785F, "PLN", DateTime.Now, "Przelew");
-            transakcja2.Write(listBox1);
-            listBox1.Items.Add("");
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
+            //Form2 form2 = new Form2();
+            //form2.Show();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            var transakcja = new Transakcja(textBox1.Text, textBox2.Text, GetInputValidator.ConvertToFloat(textBox3.Text), comboBox1.Text, GetInputValidator.GetOnlyDDMMYYY(dateTimePicker1), textBox4.Text);
+            //var transakcja = new Transakcja(textBox2.Text, GetInputValidator.ConvertToFloat(textBox3.Text), comboBox1.Text, GetInputValidator.GetOnlyDDMMYYY(dateTimePicker1), textBox4.Text);
+            //transakcja.Write(listBox1);
+            //listBox1.Items.Add("");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Transakcja transakcja = new Transakcja("Błażej", "Domagała", 100F);
+            transakcja.Write(listBox1);
+            listBox1.Items.Add("");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            Transakcja transakcja = new Transakcja(textBox1_1.Text, textBox1_2.Text, Transakcja.ConvertToFloat(textBox1_3.Text));
             transakcja.Write(listBox1);
             listBox1.Items.Add("");
         }
