14296fbacb5c2fb82b8fc7a44eff0bd1e6346b07
Błażej Domagała <petitoff@outlook.com>
2022-05-20 19:27:59 +0200

    Przesyłanie konstruktorem danych z klasy pochodnej do bazowej

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/StandardTransfer.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/StandardTransfer.cs
@@ -10,6 +10,11 @@ namespace BłażejDomagała_Bankowość
     class StandardTransfer : Transaction
     {
         private List<string> Categories = new List<string>{"Jedzenie", "Zakupy","Paliwo"};
+        public StandardTransfer(string endUserFirstName, string endUserLastName) : base(endUserFirstName, endUserLastName)
+        {
+
+        }
+
         public override void Write(ListBox listBox)
         {
             base.Write(listBox);
