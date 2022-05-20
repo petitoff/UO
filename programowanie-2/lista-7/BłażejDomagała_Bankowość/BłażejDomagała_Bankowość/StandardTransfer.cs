e51f97a140488ca5c64776333095dc770c0dd637
Błażej Domagała <petitoff@outlook.com>
2022-05-20 18:50:38 +0200

    Dodanie metody Write jako override

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/StandardTransfer.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/StandardTransfer.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BłażejDomagała_Bankowość
 {
     class StandardTransfer : Transaction
     {
-
+        public override void Write(ListBox listBox)
+        {
+            base.Write(listBox);
+            listBox.Items.Add("");
+        }
     }
 }
