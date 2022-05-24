0c2e58a1f528f1001569f96e063408541a875200
Błażej Domagała <petitoff@outlook.com>
2022-05-24 19:57:50 +0200

    Update Form4.cs

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form4.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form4.cs
@@ -15,6 +15,7 @@ namespace BłażejDomagała_Bankowość
         public Form4()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
