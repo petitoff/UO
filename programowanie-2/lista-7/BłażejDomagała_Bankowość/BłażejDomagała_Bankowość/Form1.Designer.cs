0b2d5c8af5fbd22d9e09e796f1e14fd67d6b9bc1
Błażej Domagała <petitoff@outlook.com>
2022-05-24 13:54:01 +0200

    Zmiana napisu na przycisku na "uzupełnij"

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.Designer.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form1.Designer.cs
@@ -150,7 +150,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(125, 34);
             this.button5.TabIndex = 11;
-            this.button5.Text = "Wypełnij dane testowe";
+            this.button5.Text = "Uzupełnij";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
