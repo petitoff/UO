8fcd527fd5a9ad5c54b0098ed1bab5f5d51831ef
Błażej Domagała <petitoff@outlook.com>
2022-05-26 18:15:52 +0200

    Deklaracja metody CheckIsCorrect jako klasa override

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/ScheduledTransfer.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/ScheduledTransfer.cs
@@ -19,5 +19,35 @@ namespace BłażejDomagała_Bankowość
             this.amount = amount;
             this.dateOfTransfer = dateOfTransfer;
         }
+
+        public override void CheckIsCorrect()
+        {
+            base.CheckIsCorrect();
+            if (!base.isCorrect) return; // jeżeli nie przebiegła pomyślnie to wstrzymaj działanie programu
+
+            if (amount == 0)
+            {
+                // jeżeli użytkownik próbuje dokonać przelewu na kwotę 0 to wyrzuć wyjątek
+                MessageTemplate(3);
+                return;
+            }
+
+            if (!base.CheckIsThereEnoughMoney(amount))
+            {
+                MessageTemplate(1);
+                return;
+            }
+
+            if (dateOfTransfer.Day - DateTime.Now.Day <= 1)
+            {
+                MessageTemplate(6);
+                return;
+            }
+
+            base.AmountMoneyInAccount -= amount;
+            Form1.AmountMoneyInAccount = base.AmountMoneyInAccount;
+            isCorrectSt = true;
+        }
+
     }
 }
