75ab22ee6b417cf50832db08179e62ef64c09b97
Błażej Domagała <petitoff@outlook.com>
2022-05-26 18:14:56 +0200

    Deklaracja konstruktorów wieloargumentowych

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/ScheduledTransfer.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/ScheduledTransfer.cs
@@ -13,5 +13,11 @@ namespace BłażejDomagała_Bankowość
         private float amount;
         private bool isCorrectSt;
         private DateTime dateOfTransfer;
+
+        public ScheduledTransfer(float amount, DateTime dateOfTransfer, string endUserFirstName, string endUserLastName, int endUserId, string description, Bitmap imageBitmap) : base(endUserFirstName, endUserLastName, endUserId, description, imageBitmap)
+        {
+            this.amount = amount;
+            this.dateOfTransfer = dateOfTransfer;
+        }
     }
 }
