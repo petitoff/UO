3a16fd6d0307d554e6823b5a09685dc9c5f5ca31
Błażej Domagała <petitoff@outlook.com>
2022-05-26 18:06:52 +0200

    Obsługa kliknięcia w przycisk
    Realizacja transferu

--- programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form3.cs
+++ programowanie-2/lista-7/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form3.cs
@@ -16,5 +16,21 @@ namespace BłażejDomagała_Bankowość
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int temp1 = Transaction.ConvertToInt(textBox3.Text);
+            if (temp1 == -1) return;
+
+            float temp2 = Transaction.ConvertToFloat(textBox4.Text);
+            if (temp2 == -1) return;
+
+            ScheduledTransfer scheduledTransfer = new ScheduledTransfer(temp2, dateTimePicker1.Value, textBox1.Text, textBox2.Text, temp1,
+                textBox5.Text, (Bitmap)pictureBox1.Image);
+            //standardTransfer.CheckIsCorrect();
+            Program.Transactions.Add(scheduledTransfer);
+            Program.Transactions[Program.Transactions.Count - 1].CheckIsCorrect();
+            Program.Transactions[Program.Transactions.Count - 1].Write(listBox1);
+        }
     }
 }
