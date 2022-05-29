664b4652efeeae89cf27c1d934fda9612bc6623d
Błażej Domagała <petitoff@outlook.com>
2022-05-29 23:21:46 +0200

    Rozwiązanie zadania 5

---
+++ assembly/lista 4/zadanie-5/zadanie-5/zadanie-5.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+
+const int n = 6;
+int koniec;
+int tablica[n] = { 1, 2, 3, 4, 5 };
+int suma;
+
+int main()
+{
+	__asm
+	{
+
+		mov EBX, 0
+		xor EAX, EAX
+		xor EDX, EDX
+
+		mov EAX, n
+		mov ECX, 4
+		mul ECX
+		add EAX, 4
+		mov koniec, EAX
+
+		p1 : xor EAX, EAX
+			 xor EDX, EDX
+
+			 mov EAX, tablica[EBX]
+			 mov ECX, 5
+			 div ECX
+
+			 cmp EAX, 0
+			 jne spr
+
+
+			 mov ECX, suma
+			 mov EAX, tablica[EBX]
+			 add ECX, EAX
+			 mov suma, ECX
+
+			 spr :
+		cmp EBX, koniec
+			jz koniecP
+			add EBX, 4
+			jmp p1
+			koniecP :
+	}
+
+	std::cout << suma << std::endl;
+	std::cout << koniec;
+	std::cin.get();
+}
