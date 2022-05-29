a1425a8bb8a6aed2f2d9127ce0365f8b97726f12
Błażej Domagała <petitoff@outlook.com>
2022-05-29 18:31:39 +0200

    Rozwiązanie zadania 2 - (c)

---
+++ assembly/lista 4/zadanie-2-c/zadanie-2-c/zadanie-2-c.cpp
@@ -0,0 +1,47 @@
+﻿
+#include "stdafx.h"
+
+__int16 a = 3;
+__int16 b = 5;
+
+__int16 c = 0;
+__int16 d = 6;
+
+__int16 licznik;
+__int16 mianownik;
+
+int main()
+{
+	__asm
+	{
+		// dzielenie
+
+		xor EAX, EAX
+		xor EDX, EDX
+		xor ECX, ECX
+
+		// jeżeli c == 0 to zakończ program ponieważ 0 nie może być w mianowniku
+		mov AX, c
+		cmp AX, 0
+		jz koniec
+
+		mov AX, a
+		mov CX, d
+		mul CX
+
+		mov licznik, AX
+		
+		mov AX, b
+		mov CX, c
+		mul CX
+
+		mov mianownik, CX
+
+		koniec :
+
+	}
+
+	std::cout << "licznik: " << licznik << std::endl;
+	std::cout << "mianownik: " << mianownik;
+	std::cin.get();
+}
