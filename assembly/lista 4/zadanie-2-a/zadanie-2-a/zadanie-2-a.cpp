bfa60b7822a9dfc841ebeed0d1616ef531b37ea0
Błażej Domagała <petitoff@outlook.com>
2022-05-29 18:11:27 +0200

    Rozwiązanie do zadania 2 podpunkt a

---
+++ assembly/lista 4/zadanie-2-a/zadanie-2-a/zadanie-2-a.cpp
@@ -0,0 +1,47 @@
+
+#include "stdafx.h"
+
+__int16 a = 3;
+__int16 b = 5;
+
+__int16 c = 4;
+__int16 d = 6;
+
+__int16 licznik;
+__int16 mianownik;
+
+int main()
+{
+	__asm
+	{
+		// dodawanie
+		xor EAX, EAX
+		xor EDX, EDX
+		xor ECX, ECX
+
+		mov AX, b
+		mov CX, d
+		mul CX
+
+		mov mianownik, AX
+
+		mov AX, a
+		mov CX, d
+		mul CX
+		
+		push AX
+
+		mov AX, c
+		mov CX, b
+		mul CX
+
+		pop CX
+
+		add AX, CX
+		mov licznik, AX
+	}
+	
+	std::cout << "licznik: " << licznik <<std::endl;
+	std::cout << "mianownik: " << mianownik;
+	std::cin.get();
+}
