b09ebe917a13f2cb4dccdf3a8b0a09ca14156344
Błażej Domagała <petitoff@outlook.com>
2022-05-30 17:49:00 +0200

    Wyznaczanie rzędu macierza 2x2

---
+++ octave/rzedyDwieKolumny.m
@@ -0,0 +1,53 @@
+function wynik = rzedyDwieKolumny(m1)
+
+  # a - wiersze
+  # b - kolumny
+
+  [a,b] = size(m1);
+  spr1 = 0;
+  spr2 = 0;
+
+  # sprawdzanie czy podana macierz posiada dwie kolumny
+  if(b != 2)
+    wynik = "Podałeś nieprawidołową macierz!";
+    return;
+  endif
+
+  wierszeG = 1;
+  wierszeD = 2;
+
+  while(wierszeD < a)
+
+    wyznacznikMacierza2x2 = m1(wierszeG,1) * m1(wierszeD,2) - m1(wierszeG,2) * m1(wierszeD,1);
+    if(wyznacznikMacierza2x2 != 0)
+      spr2 = 1;
+    endif
+
+    if(spr2 == 0)
+      for i=1:a
+        if(m1(i,1)!=0)
+          spr1=1;
+        endif
+        if(m1(i,2)!=0)
+          spr1=1;
+        endif
+      endfor
+    endif
+
+    wierszeG = wierszeG + 1;
+    wierszeD = wierszeD + 1;
+  endwhile
+
+  if(spr2 == 1)
+    wynik = 2;
+    return;
+  endif
+
+  if(spr1 == 1)
+    wynik = 1;
+    return;
+  else
+    wynik = 0;
+    return;
+  endif
+endfunction
