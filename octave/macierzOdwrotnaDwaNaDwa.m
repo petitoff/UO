d75b5cdb3700e8bdf76a4f782eda29732116813b
Błażej Domagała <petitoff@outlook.com>
2022-05-30 18:15:34 +0200

    Obliczanie macierzy odwrotnej 2x2 jeżeli istnieje

---
+++ octave/macierzOdwrotnaDwaNaDwa.m
@@ -0,0 +1,27 @@
+function wynik = macierzOdwrotnaDwaNaDwa(m1)
+  [a,b] = size(m1);
+
+  if(a != 2 && b != 2)
+    wynik = "Macierz nie jest 2x2 więc macierz odwrotna nieistnieje";
+    return;
+  endif
+
+  # obliczanie wyznacznika
+  wyznacznik = m1(1,1) * m1(2,2) - m1(1,2) * m1(2,1);
+
+  if(wyznacznik == 0)
+    wynik = "Macierz odwrotna nie istnieje";
+    return;
+  endif
+
+  # obliczanie macierzy odwrotnej
+  wynik = [m1(2,2) -m1(1,2); -m1(2,1) m1(1,1)];
+
+  for i=1:a
+    for j=1:b
+      wynik(i,j) *= 1/wyznacznik;
+    endfor
+  endfor
+
+
+endfunction
