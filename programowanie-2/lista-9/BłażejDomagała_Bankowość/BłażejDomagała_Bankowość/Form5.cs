ba201e7baf3b0e6d8c600d34313b7fa40a2c4f5f
Błażej Domagała <petitoff@outlook.com>
2022-06-03 14:39:21 +0200

    lista-9

---
+++ programowanie-2/lista-9/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form5.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BłażejDomagała_Bankowość
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            float temp1 = Transaction.ConvertToFloat(numericUpDown1.Text);
+            float temp2 = Transaction.ConvertToFloat(numericUpDown2.Text);
+            if (temp1 == -1 || temp2 == -1)
+            {
+                return;
+            }
+
+            StandardTransfer standardTransfer1;
+            StandardTransfer standardTransfer2 = new StandardTransfer(temp1);
+            StandardTransfer standardTransfer3 = new StandardTransfer(temp2);
+
+            standardTransfer1 = standardTransfer2 * standardTransfer3;
+
+            listBox1.Items.Add($"Zarobisz: {standardTransfer1.a} PLN");
+            listBox1.Items.Add("");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int temp1 = Transaction.ConvertToInt(numericUpDown3.Text);
+            int temp2 = Transaction.ConvertToInt(numericUpDown4.Text);
+
+            if (temp1 == -1 || temp2 == -2) return;
+
+            StandardTransfer standardTransfer1 = new StandardTransfer(temp1);
+            StandardTransfer standardTransfer2 = new StandardTransfer(temp2);
+            StandardTransfer standardTransfer;
+
+            standardTransfer = standardTransfer1 == standardTransfer2;
+
+            listBox1.Items.Add($"{(standardTransfer.b ? "Możesz dokonać przelewu!" : "Nie możesz dokonać przelewu!")}");
+            listBox1.Items.Add("");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float temp1 = Transaction.ConvertToFloat(numericUpDown6.Text);
+            float temp2 = Transaction.ConvertToFloat(numericUpDown5.Text);
+
+            if (temp1 == -1 || temp2 == -2) return;
+
+            StandardTransfer standardTransfer1 = new StandardTransfer(temp1);
+            StandardTransfer standardTransfer2 = new StandardTransfer(temp2);
+            StandardTransfer standardTransfer;
+
+            standardTransfer = standardTransfer1 != standardTransfer2;
+
+            listBox1.Items.Add($"{(standardTransfer.b ? "Ceny są takie same!" : "Ceny nie są takie same!")}");
+            listBox1.Items.Add("");
+        }
+    }
+}
---
+++ programowanie-2/lista-9/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/StandardTransfer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BłażejDomagała_Bankowość
+{
+    class StandardTransfer : Transaction
+    {
+        //private List<string> Categories = new List<string> { "Jedzenie", "Zakupy", "Paliwo" };
+        private float amount;
+        private string currency;
+        private float usdPrice = 3.5F;
+        private float eurPrice = 4.3F;
+        private string categories;
+        private bool isCorrectSt = false;
+
+        public float a;
+        public bool b;
+
+        public StandardTransfer()
+        {
+
+        }
+
+        public StandardTransfer(float a)
+        {
+            this.a = a;
+        }
+
+        public StandardTransfer(string endUserFirstName, string endUserLastName, int endUserId, float amount, string currency, string description, string categories, Bitmap imageBitmap, float AmountMoneyInAccount) : base(endUserFirstName, endUserLastName, endUserId, description, imageBitmap)
+        {
+            this.amount = amount;
+            this.currency = currency;
+            this.categories = categories;
+        }
+
+        private bool ConvertCurrency()
+        {
+
+            if (currency == "PLN")
+            {
+                if (!base.CheckIsThereEnoughMoney(amount))
+                {
+                    MessageTemplate(1);
+                    return false;
+                }
+                return true;
+            }
+
+            if (currency == "USD")
+            {
+                if (!base.CheckIsThereEnoughMoney(amount * usdPrice))
+                {
+                    MessageTemplate(1);
+                    return false;
+                }
+                return true;
+            }
+
+            if (currency == "EUR")
+            {
+                if (!base.CheckIsThereEnoughMoney(amount * eurPrice))
+                {
+                    MessageTemplate(1);
+                    return false;
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void CheckIsCorrect()
+        {
+            base.CheckIsCorrect(); // walidacja z kalsy bazowej
+            if (!base.isCorrect) return; // jeżeli nie przebiegła pomyślnie to wstrzymaj działanie programu
+
+            if (amount == 0)
+            {
+                // jeżeli użytkownik próbuje dokonać przelewu na kwotę 0 to wyrzuć wyjątek
+                MessageTemplate(3);
+                return;
+            }
+            if (!ConvertCurrency()) return;
+
+            base.AmountMoneyInAccount -= amount;
+            Form1.AmountMoneyInAccount = base.AmountMoneyInAccount;
+            isCorrectSt = true;
+        }
+
+        private DateTime WhenTransactionWillBeCompleted()
+        {
+            return DateTime.Now.AddDays(2).Date;
+        }
+
+        public override void Write(ListBox listBox)
+        {
+            if (!isCorrectSt)
+            {
+                MessageTemplate(2);
+                listBox.Items.Add("Przelew się nieodbyl! Pieniądze nie zostały pobrane!");
+                return;
+            }
+
+            if (!base.isCorrect) return;
+            base.Write(listBox);
+            listBox.Items.Add($"Kwota transakcji: {amount} {currency}");
+            listBox.Items.Add($"Przelew zostanie zrealizowany w ciągu 2 dni roboczych: {WhenTransactionWillBeCompleted():dd/MM/yyyy}");
+            listBox.Items.Add($"Kategoria przelewu: {categories}");
+            listBox.Items.Add($"Typ przelewu: Przelew zwykły");
+            listBox.Items.Add("");
+        }
+
+        public static StandardTransfer operator ==(StandardTransfer standardTransfer1,
+            StandardTransfer standardTransfer2)
+        {
+            StandardTransfer standardTransfer = new StandardTransfer();
+            if (standardTransfer1.a == standardTransfer2.a) standardTransfer.b = false;
+            else standardTransfer.b = true;
+
+            return standardTransfer;
+        }
+
+        public static StandardTransfer operator !=(StandardTransfer standardTransfer1,
+            StandardTransfer standardTransfer2)
+        {
+            float usdPrice = 3.5f;
+            StandardTransfer standardTransfer = new StandardTransfer();
+
+            if ((standardTransfer1.a * usdPrice) == standardTransfer2.a) standardTransfer.b = true;
+            else standardTransfer.b = false;
+
+            return standardTransfer;
+        }
+
+        public static StandardTransfer operator *(StandardTransfer standardTransfer1,
+            StandardTransfer standardTransfer2)
+        {
+            StandardTransfer sum = new StandardTransfer();
+            sum.a = standardTransfer1.a * (standardTransfer2.a / 100);
+
+            return sum;
+        }
+    }
+}
