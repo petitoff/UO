d1a131ddc81a7e56a5e9308d67c21d21ad4b7436
Błażej Domagała <petitoff@outlook.com>
2022-06-04 13:18:06 +0000

    Update Form4.Designer.cs

--- programowanie-2/lista-9/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form4.Designer.cs
+++ programowanie-2/lista-9/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form4.Designer.cs
@@ -33,6 +33,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(330, 376);
+            this.button1.Location = new System.Drawing.Point(143, 312);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 33);
             this.button1.TabIndex = 1;
@@ -57,15 +58,15 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(485, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(453, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(245, 144);
+            this.pictureBox1.Size = new System.Drawing.Size(245, 131);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(226, 376);
+            this.button2.Location = new System.Drawing.Point(39, 312);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(98, 33);
             this.button2.TabIndex = 3;
@@ -75,7 +76,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(485, 376);
+            this.button3.Location = new System.Drawing.Point(298, 312);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(98, 33);
             this.button3.TabIndex = 4;
@@ -83,11 +84,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(719, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Zapisz do pliku";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(858, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
@@ -108,5 +120,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
