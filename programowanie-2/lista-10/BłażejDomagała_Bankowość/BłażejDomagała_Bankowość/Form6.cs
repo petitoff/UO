76ae15678975bcd0d3ea0281a35c4cbf70b9c4d2
Błażej Domagała <petitoff@outlook.com>
2022-06-04 17:13:24 +0000

    Ilustrowanie operacji na wszystkich obiektach z listy

---
+++ programowanie-2/lista-10/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BłażejDomagała_Bankowość
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float sum = 0;
+            foreach (Transaction transaction in Program.Transactions)
+            {
+                sum += transaction.GetAmount();
+            }
+
+            listBox1.Items.Add($"Ilość wszystkich twoich transakcji to: {sum} PLN");
+        }
+    }
+}
