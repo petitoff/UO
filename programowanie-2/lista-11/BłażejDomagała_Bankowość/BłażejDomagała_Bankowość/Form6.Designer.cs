3d76f9ec45605cb9fe550f5bcba7ddf271e0f469
Błażej Domagała <petitoff@outlook.com>
2022-06-06 18:12:01 +0200

    Zwiększenie czcionki dla poprawy czytelności

--- programowanie-2/lista-11/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form6.Designer.cs
+++ programowanie-2/lista-11/BłażejDomagała_Bankowość/BłażejDomagała_Bankowość/Form6.Designer.cs
@@ -34,11 +34,13 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(776, 238);
+            this.listBox1.Size = new System.Drawing.Size(776, 224);
             this.listBox1.TabIndex = 0;
             // 
             // button1
