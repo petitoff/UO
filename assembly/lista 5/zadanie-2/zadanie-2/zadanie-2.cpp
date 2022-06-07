71f46eb3438b2de524800b5021e5f4cfef9bcc94
Błażej Domagała <petitoff@outlook.com>
2022-06-07 17:20:47 +0200

    Przygotowanie liczby do sprawdzania

--- assembly/lista 5/zadanie-2/zadanie-2/zadanie-2.cpp
+++ assembly/lista 5/zadanie-2/zadanie-2/zadanie-2.cpp
@@ -1,15 +1,27 @@
 #include "stdafx.h"
 
 double liczba = 1.005;
-double wynik;
+double m = 1000;
+int wynik;
+
 
 int main()
 {
 	__asm
 	{
-		movzx EAX, liczba
+		// movzx EAX, liczba
+		FLD liczba;
+		FMUL m;
 
+		FIST wynik;
 
+		xor EAX, EAX
+		xor EBX, EBX
+
+		mov EAX, wynik
+		
+
+		mov wynik, EAX
 	}
 	std::cout << wynik;
 	std::cin.get();
