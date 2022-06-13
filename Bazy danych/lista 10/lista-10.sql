6a8ab5c8c4eb074bd6661dff2519d191f937730f
Błażej Domagała <petitoff@outlook.com>
2022-06-13 13:41:16 +0200

    Zadanie 9

--- Bazy danych/lista 10/lista-10.sql
+++ Bazy danych/lista 10/lista-10.sql
@@ -9,3 +9,9 @@ SELECT *
 FROM employees
 WHERE ReportsTo IS NULL;
 
+-- zadanie 9
+SELECT e1.EmployeeID, e1.LastName, e1.FirstName, e2.EmployeeID as KierownikID, e2.LastName, e2.FirstName
+FROM employees as e1 INNER JOIN employees as e2 ON e1.ReportsTo = e2.EmployeeID;
+
+SELECT *
+FROM employees;
