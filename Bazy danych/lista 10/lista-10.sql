53bb77ab8ecceb17c53e4c7657464c8c93ff2dda
Błażej Domagała <petitoff@outlook.com>
2022-06-13 13:41:09 +0200

    Update lista-10.sql

--- Bazy danych/lista 10/lista-10.sql
+++ Bazy danych/lista 10/lista-10.sql
@@ -1,5 +1,4 @@
 -- zadanie 7
-
 SELECT products.ProductID, products.ProductName, categories.CategoryName, suppliers.Country
 FROM products, categories, suppliers
 WHERE categories.CategoryName = "Beverages" AND suppliers.Country = "USA"
