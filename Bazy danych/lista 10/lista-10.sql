eb9366e526ee80dd8c4a570e75b9168e3c1edd39
Błażej Domagała <petitoff@outlook.com>
2022-06-14 18:59:33 +0200

    Update lista-10.sql

--- Bazy danych/lista 10/lista-10.sql
+++ Bazy danych/lista 10/lista-10.sql
@@ -54,6 +54,8 @@ GROUP BY OrderID;
 -- zadanie 16
 SELECT EmployeeID, LastName, FirstName
 FROM employees
-WHERE EmployeeID NOT IN(SELECT ReportsTo FROM employees WHERE ReportsTo IS NOT null);WHERE EmployeeID NOT IN(SELECT ReportsTo FROM employees WHERE ReportsTo IS NOT null);
+WHERE EmployeeID NOT IN(SELECT ReportsTo FROM employees WHERE ReportsTo IS NOT null);
+
+-- zadanie 17
 SELECT suppliers.SupplierID, suppliers.ContactName
 FROM suppliers Inner JOIN (SELECT SupplierID FROM products INNER JOIN orderdetails on products.ProductID = orderdetails.ProductID where orderdetails.OrderID > 11000) as t2 on suppliers.SupplierID = t2.SupplierID;
