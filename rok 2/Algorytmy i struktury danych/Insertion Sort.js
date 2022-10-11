bafe7a8c2b8a57c5b53f5eace57ab05f2e6c12fb
Błażej Domagała <petitoff@outlook.com>
2022-10-11 18:39:44 +0200

    update

---
+++ rok 2/Algorytmy i struktury danych/Insertion Sort.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * @param {array} arr
+ * @returns {array}
+ */
+
+const insertionSort = (arr = []) => {
+  let numbersOfComparisons = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    const curr = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > curr) {
+      numbersOfComparisons++;
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = curr;
+  }
+
+  console.log(`Liczba porównań: ${numbersOfComparisons}`);
+  console.log(
+    `Średnia wartość: ${
+      Math.round((numbersOfComparisons / Math.pow(arr.length, 2)) * 100) / 100
+    }`
+  );
+
+  return arr;
+};
+
+const arr = Array.from({ length: 10 }, () => Math.floor(Math.random() * 1000));
+console.log(insertionSort([5, 4, 3, 2, 1]));
+
+// S: O(1)
+// T: O(n^2)
