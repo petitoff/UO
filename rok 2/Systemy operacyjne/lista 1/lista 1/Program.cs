0b20848deef8f7dd7d399824a895b513d63e05dc
Błażej Domagała <petitoff@outlook.com>
2022-10-12 18:10:09 +0200

    zadanie 4

---
+++ rok 2/Systemy operacyjne/lista 1/lista 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lista_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintSquares);
+            thread.Start();
+
+            //thread.Join();
+
+            Thread thread2 = new Thread(PrintQuobes);
+            thread2.Start();
+
+            //PrintQuobes();
+
+
+            //PrintSquares();
+            PintHelloFriend();
+
+            Console.ReadKey();
+        }
+
+        static void PrintSquares()
+        {
+            var max = 100;
+
+            for(int i = 1; i < max; i++)
+            {
+                Console.WriteLine((i * i).ToString());
+            }
+        }
+
+        static void PrintQuobes()
+        {
+            var max = 100;
+
+            for (int i = 1; i < max; i++)
+            {
+                Console.WriteLine((i * i * i).ToString());
+            }
+        }
+
+        static void PintHelloFriend()
+        {
+            for(int i = 0; i <100; i++)
+            {
+                Console.WriteLine("Hello my friend");
+            }
+        }
+    }
+}
