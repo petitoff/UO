627b24d319932bbe714ebb33a15fa3290e4cf1b6
Błażej Domagała <petitoff@outlook.com>
2022-11-09 18:44:09 +0100

    added L2.1

---
+++ rok 2/Systemy operacyjne/L2.1/L2.1/L2.1/Calc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L2._1
+{
+    internal class Calc
+    {
+        private const int loop = 10000;
+        // create list from 0 to 100 with 0
+        List<int> list = Enumerable.Repeat(0, 100).ToList();
+
+        public void Main()
+        {
+            var thread1 = new Thread(Add);
+            var thread2 = new Thread(Subtract);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            // max number from list
+            Console.WriteLine("Max number from list: " + list.Max());
+            // min number from list
+            Console.WriteLine("Min number from list: " + list.Min());
+
+            try
+            {
+                // save list to txt
+                System.IO.File.WriteAllLines(@"C:\Users\petit\Desktop\repos\UO\rok 2\Systemy operacyjne\L2.1\WriteLines.txt", list.Select(x => x.ToString()));
+            }
+            catch (System.UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                {
+                    Console.WriteLine(e);
+                    return;
+                }
+            }
+        }
+
+        void Add()
+        {
+            Random random = new Random();
+            for (int i = 0; i < loop; i++)
+            {
+                // random number from 0 to 100
+                int randomNumber = random.Next(0, 100);
+                list[randomNumber] += 1;
+            }
+        }
+
+        void Subtract()
+        {
+            Random random = new Random();
+            for (int i = 0; i < loop; i++)
+            {
+                // random number from 0 to 100
+                int randomNumber = random.Next(0, 100);
+                list[randomNumber] -= 1;
+            }
+        }
+    }
+}
---
+++ rok 2/Systemy operacyjne/L2.1/L2.1/L2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L2._1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Calc calc = new Calc();
+            calc.Main();
+        }
+    }
+}
