df58a950ddfa1e8a2c7c04dd36755a335034073c
Błażej Domagała <petitoff@outlook.com>
2022-11-09 18:45:34 +0100

    fixed name of solution and project

--- rok 2/Systemy operacyjne/L2.1/L2.1/L2.1/Calc.cs
+++ rok 2/Systemy operacyjne/L2.1/L2.1/L2.1/Calc.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace L2._1
+namespace Z1
 {
     internal class Calc
     {
