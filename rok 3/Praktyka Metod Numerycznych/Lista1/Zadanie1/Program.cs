07cd663f609a15cce8137838e52d92775a5c3762
Błażej Domagała <petitoff@outlook.com>
2023-10-02 19:46:58 +0200

    lista 1

---
+++ rok 3/Praktyka Metod Numerycznych/Lista1/Zadanie1/Program.cs
@@ -0,0 +1,33 @@
+﻿float a = 0.1f;
+float b = 0.2f;
+float c = 0.3f;
+
+bool additionAssociativitySinglePrecision = (a + (b + c)) == ((a + b) + c);
+bool multiplicationAssociativitySinglePrecision = (a * (b * c)) == ((a * b) * c);
+
+Console.WriteLine((a + (b + c)));
+Console.WriteLine(((a + b) + c));
+
+Console.WriteLine((a * (b * c)));
+Console.WriteLine(((a * b) * c));
+
+double x = 0.1;
+double y = 0.2;
+double z = 0.3;
+
+bool additionAssociativityDoublePrecision = (x + (y + z)) == ((x + y) + z);
+bool multiplicationAssociativityDoublePrecision = (x * (y * z)) == ((x * y) * z);
+
+Console.WriteLine((x + (y + z)));
+Console.WriteLine(((x + y) + z));
+
+Console.WriteLine((x * (y * z)));
+Console.WriteLine(((x * y) * z));
+
+Console.WriteLine("Associativity check for single precision:");
+Console.WriteLine("Addition: " + additionAssociativitySinglePrecision);
+Console.WriteLine("Multiplication: " + multiplicationAssociativitySinglePrecision);
+
+Console.WriteLine("\nAssociativity check for double precision:");
+Console.WriteLine("Addition: " + additionAssociativityDoublePrecision);
+Console.WriteLine("Multiplication: " + multiplicationAssociativityDoublePrecision);
---
+++ rok 3/Praktyka Metod Numerycznych/Lista1/Zadanie5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = 1.0;  // Wartość, dla której chcesz obliczyć e^x
+        double epsilon = 0.0001;  // Tolerancja błędu
+        double result = CalculateExponential(x, epsilon);
+        Console.WriteLine(result);
+    }
+
+    static double CalculateExponential(double x, double epsilon)
+    {
+        double sum = 1.0;  // Początkowa suma (pierwszy składnik szeregu Taylora)
+        double term = 1.0;  // Początkowy składnik szeregu Taylora
+        int n = 1;  // Licznik składników
+
+        // Kontynuuj dodawanie składników, aż różnica będzie mniejsza niż epsilon
+        while (true)
+        {
+            term *= x / n;  // Oblicz kolejny składnik szeregu
+            double newSum = sum + term;  // Oblicz nową sumę
+
+            // Sprawdź, czy różnica jest mniejsza niż epsilon
+            if (Math.Abs(newSum - sum) < epsilon)
+                break;
+
+            sum = newSum;  // Zaktualizuj sumę
+            n++;  // Zwiększ licznik składników
+        }
+
+        return sum;
+    }
+}
