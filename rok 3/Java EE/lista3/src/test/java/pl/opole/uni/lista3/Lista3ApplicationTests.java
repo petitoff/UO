0b9a0888ce14a45bf56d124bfe28bc817b61ec9a
Błażej Domagała <petitoff@outlook.com>
2024-04-11 14:17:16 +0200

    lista3

---
+++ rok 3/Java EE/lista3/src/test/java/pl/opole/uni/lista3/Lista3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.opole.uni.lista3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
